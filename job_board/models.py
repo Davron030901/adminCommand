cd2c2aa87fce0a620f88e07d3c7ed819bf72c413
TAMKEEN <aliqulovdavron11@gmail.com>
2024-03-31 11:04:22 -0700

    Job Board

---
+++ job_board/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class JobPosting(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    company = models.CharField(max_length=100)
+    salary = models.IntegerField()
+    is_active = models.BooleanField(default=False)
